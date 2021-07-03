34ae0d32bdf163e361dc16e9c2379dc61e63524f
matifandy <matifandy@gmail.com>
2021-07-03 15:08:45 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  // name: 'App',
+  // components: {
+  // }
 }
 </script>
 
-<style>
+<style  scoped lang="scss">
+    @import "@/assets/variables.scss";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
